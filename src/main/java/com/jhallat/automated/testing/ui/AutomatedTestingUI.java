e7ec7e9ca2a73ce1882075e4d77fc9634ec0259a
John Hallat <jhallat@gmail.com>
2019-03-05 22:26:22 -0500

    Added file menu

--- src/main/java/com/jhallat/automated/testing/ui/AutomatedTestingUI.java
+++ src/main/java/com/jhallat/automated/testing/ui/AutomatedTestingUI.java
@@ -2,6 +2,9 @@ package com.jhallat.automated.testing.ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -22,7 +25,21 @@ public class AutomatedTestingUI extends Application {
     private Scene createScene() {
 
         BorderPane mainForm = new BorderPane();
+        MenuBar menuBar = createMenuBar();
+
+        mainForm.setTop(menuBar);
 
         return new Scene(mainForm, 1200, 800);
+    }
+
+    private MenuBar createMenuBar() {
+        Menu fileMenu = new Menu("File");
+        MenuItem newProjectMenuItem = new MenuItem("New Test Project");
+        fileMenu.getItems().addAll(newProjectMenuItem);
+        
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(fileMenu);
+
+        return menuBar;
     }
 }
